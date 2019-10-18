e09d512aaf718b808f7a4c760e9e96f7a0b5c2fd
Yuri Kimura <yuri.kimura@luxiar.com>
2019-10-18 18:20:00 +0900

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_10_18_042643) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.integer "page"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
