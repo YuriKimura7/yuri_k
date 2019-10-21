d701e39bdf7860deb560a6bc766c3b4dbeb7c642
Yuri Kimura <yuri.kimura@luxiar.com>
2019-10-21 15:13:38 +0900

    課題6:本の情報を画面から登録できるようにしよう

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,4 +5,12 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+  def new
+    @book = Book.new
+  end
+  def create
+    @book = Book.new(params.require(:book).permit(:name, :price, :page))
+    @book.save
+    redirect_to books_path
+  end
 end
