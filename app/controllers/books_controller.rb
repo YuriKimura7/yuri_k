485fee5a30d937eb6a54edee30d1b80cbc079282
Yuri Kimura <yuri.kimura@luxiar.com>
2019-10-21 14:37:44 +0900

    課題5:単価メソッド追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,10 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new
   end
   def show
     @book = Book.find(params[:id])
+    @book1 = Book.new
   end
 end
