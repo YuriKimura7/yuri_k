cf3156672d688479fbb62786dc06a3b605590b49
Yuri Kimura <yuri.kimura@luxiar.com>
2019-10-21 10:58:03 +0900

    課題4:本の詳細情報を表示しよう

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
-
   def show
+    @book = Book.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'books/index'
+  resources :books, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
