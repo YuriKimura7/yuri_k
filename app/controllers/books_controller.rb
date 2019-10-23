a27109d6ddd218d100747378a73bbb69a81f5fe7
Yuri Kimura <yuri.kimura@luxiar.com>
2019-10-23 10:59:23 +0900

    課題7修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,31 +2,32 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
+
   def show
-    @book = find_book_by_id
+    @book = Book.find(params[:id])
   end
+
   def new
     @book = Book.new
   end
+
   def create
     @book = Book.new(book_params)
     @book.save
     redirect_to books_path
   end
+
   def edit
-    @book = find_book_by_id
+    @book = Book.find(params[:id])
   end
+
   def update
-    @book = find_book_by_id
+    @book = Book.find(params[:id])
     @book.update(book_params)
     redirect_to book_path
   end
 
   private
-  def find_book_by_id
-    Book.find(params[:id])
-  end
-
   def book_params
     params.require(:book).permit(:name, :price, :page)
   end
