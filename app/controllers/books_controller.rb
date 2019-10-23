4f4d9e2728ef94aff3c65f9289fbaf5978f02539
Yuri Kimura <yuri.kimura@luxiar.com>
2019-10-23 10:19:56 +0900

    課題7:本の情報を画面から編集できるようにしよう

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,14 +3,31 @@ class BooksController < ApplicationController
     @books = Book.all
   end
   def show
-    @book = Book.find(params[:id])
+    @book = find_book_by_id
   end
   def new
     @book = Book.new
   end
   def create
-    @book = Book.new(params.require(:book).permit(:name, :price, :page))
+    @book = Book.new(book_params)
     @book.save
     redirect_to books_path
+  end
+  def edit
+    @book = find_book_by_id
+  end
+  def update
+    @book = find_book_by_id
+    @book.update(book_params)
+    redirect_to book_path
+  end
+
+  private
+  def find_book_by_id
+    Book.find(params[:id])
+  end
+
+  def book_params
+    params.require(:book).permit(:name, :price, :page)
   end
 end
