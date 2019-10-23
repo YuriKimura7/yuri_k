497506706976cef909fd3aa229b98f256abd174e
Yuri Kimura <yuri.kimura@luxiar.com>
2019-10-23 11:24:42 +0900

    課題8: 本の登録、編集時にバリデーションをかけよう

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,8 @@
 class Book < ApplicationRecord
+  validates :name, presence: true
+  validates :price, presence: true, numericality: true
+  validates :page, presence: true, numericality: true
+
   def unit_with_tax
     price / page
   end
